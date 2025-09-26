d5331af924a246b737457f14400d398c0ee7d89e
brandon <brandon.mondoy@stud.unifi.it>
2025-09-26 05:09:05 -0400

    smallest simple created
    and started to code it. Need to be to windows to use eclipse to resolve it in java because I feel confident with Java.ArrayList

---
+++ smallestSimple/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+// Greater common multiple
+
+class Factor{
+    public:
+        int number;
+        int pow;
+};
+
+int main(){
+    
+}
