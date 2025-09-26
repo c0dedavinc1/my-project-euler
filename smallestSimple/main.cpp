dc24dff264bcbe9d27394710776aa856239632d2
brandon <brandon.mondoy@stud.unifi.it>
2025-09-26 06:06:12 -0400

    Removed all the java files
    and wrote findIndexByNumero in c++

--- smallestSimple/main.cpp
+++ smallestSimple/main.cpp
@@ -11,6 +11,19 @@ class Factor{
         int pow;
 };
 
+int findIndexByNumero(const std::vector<Factor>& factors, int x) {
+    for (size_t i = 0; i < factors.size(); i++) {
+        if (factors[i].number == x) {
+            return static_cast<int>(i); 
+        }
+    }
+    return -1; 
+}
+
+
 int main(){
+    std::vector<Factor> factorVector;
     
 }
+
+
